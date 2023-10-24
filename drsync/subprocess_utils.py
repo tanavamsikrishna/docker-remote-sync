ef4b13cc616e12ba118fc9bff8e9ddb20b2dba52
Vamsi Talupula <tanavamsikrishna@outlook.com>
2023-10-25 01:14:19 +0530

    Update error checking function

--- drsync/subprocess_utils.py
+++ drsync/subprocess_utils.py
@@ -4,9 +4,10 @@ import sys
 
 def check_subprocess_errors(error_lines):
     error_lines = [el for e in error_lines if len(el := e.rstrip()) > 0]
-    if len(error_lines) != 0:
-        print("\n".join(error_lines))
-        sys.exit(1)
+    if len(error_lines) == 0:
+        return
+    print("\n".join(error_lines), file=sys.stderr)
+    sys.exit(1)
 
 
 def start_subprocess(*args):
