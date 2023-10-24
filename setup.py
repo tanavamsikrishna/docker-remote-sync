345e6b008c85735f9c36078186782c6d35dc4d8c
Vamsi Talupula <tanavamsikrishna@outlook.com>
2023-10-25 02:20:16 +0530

    Fixing publish issue

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="docker-remote-sync",
-    version="0.1.2",
+    version="0.1.3",
     packages=find_packages(),
-    entry_points={"console_scripts": ["drsync = drsync.__main__:main"]},
+    entry_points={"console_scripts": ["drsync = drsync.drsync:main"]},
 )
