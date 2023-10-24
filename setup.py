548274c4d9dbc136b690add9321ad725fe1cc294
Vamsi Talupula <tanavamsikrishna@outlook.com>
2023-10-25 02:44:50 +0530

    removing setup.py

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-
-setup(
-    name="docker-remote-sync",
-    version="0.1.5",
-    packages=find_packages(),
-    entry_points={"console_scripts": ["drsync=drsync.drsync:main"]},
-)
