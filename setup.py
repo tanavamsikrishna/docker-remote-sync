ce184ead050b8d5329af3808b9cae2edb4b417bb
Vamsi Talupula <tanavamsikrishna@outlook.com>
2023-10-25 02:10:13 +0530

    Bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="docker-remote-sync",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(),
     entry_points={"console_scripts": ["drsync = docker-remote-sync.__main__:main"]},
 )
