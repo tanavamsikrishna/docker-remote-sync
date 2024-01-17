435ef4fea894bb402568b5bf09bda74645e5a529
Vamsi Talupula <tanavamsikrishna@outlook.com>
2024-01-17 12:15:18 +0530

    Update release script

--- .fish-functions/run.fish
+++ .fish-functions/run.fish
@@ -1,18 +1,17 @@
 function run
     switch "$argv[1]"
         case release
-            set git_status (git status --porcelain | string trim)
+            set git_status (git status --porcelain | tr -d '\n' | string trim)
             test -n $git_status; and begin
                 echo "Untracked/uncommited files exist"
                 return 1
             end
-            read -f -P "New tag: " tag
-            rg $tag pyproject.toml >/dev/null; or begin
-                echo "pyproject.toml doesn't have the correct version?"
-                return 1
-            end
-            git tag $tag
+            read -f -P "New tag (a.b.c): " tag
+            sd '^version = ".*"' 'version = "'$tag'"' pyproject.toml
+            git add .
+            git commit -m "Bump up version to"$tag
             git push
+            git tag $tag
             git push --tags
             gh release create v$tag
     end
