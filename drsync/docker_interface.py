6003b547bbda869b345e02d49ca3fc2f7905af74
Vamsi Talupula <tanavamsikrishna@outlook.com>
2024-01-20 13:31:07 +0530

    bug fix

--- drsync/docker_interface.py
+++ drsync/docker_interface.py
@@ -4,8 +4,9 @@ from drsync.io_util import print_header
 from drsync.subprocess_utils import start_subprocess
 
 
-def save_docker_image(docker_executable, image_name, file: BinaryIO):
+def save_docker_image(docker_executable: str, image_name, file: BinaryIO):
+    docker_executable = docker_executable.split()
     print_header("Reading a snapshot of the docker image")
-    process = start_subprocess(docker_executable, "save", image_name, text=False)
+    process = start_subprocess(*docker_executable, "save", image_name, text=False)
     stdout_data, _ = process.communicate()
     file.write(stdout_data)
