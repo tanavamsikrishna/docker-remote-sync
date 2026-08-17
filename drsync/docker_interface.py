1d754b51aaca1e4280c23665935a20c7b4bf04de
Vamsi Talupula <tanavamsikrishna@outlook.com>
2026-08-17 14:32:17 +0530

    fix: correct misleading log message in save_docker_image

--- drsync/docker_interface.py
+++ drsync/docker_interface.py
@@ -5,7 +5,7 @@ from drsync.subprocess_utils import start_subprocess
 
 
 def save_docker_image(docker_executable: str, image_name: str, file: BinaryIO):
-    print_header(f"Saving a snapshot of the docker image: {image_name} to {file}")
+    print_header(f"Saving a snapshot of the docker image: {image_name}")
     process = start_subprocess(*docker_executable.split(), "save", image_name, text=False)
     stdout_data, _ = process.communicate()
     _ = file.write(stdout_data)
